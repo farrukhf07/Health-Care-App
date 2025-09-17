05b77447e3da07a0f507634fe108b9bda16d6b22
Farrukh <farrukhahmad022@gmail.com>
2025-09-17 22:40:20 +0500

    Remove AIConstant.kt from repo and ignore it

--- app/src/main/java/com/example/myhealth/utils/AIConstant.kt
+++
@@ -1,6 +0,0 @@
-package com.example.myhealth.utils
-
-const val MODEL = "model"
-const val USER = "user"
-const val MODEL_NAME = "gemini-pro"
-const val API_KEY = "AIzaSyAp2qWs9Y-YJcTfuilgocxOER5T2yABPHY"
